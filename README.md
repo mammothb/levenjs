6ed970618a5cb1f00b968aeb93aa471f4184e5bc
mmb L <7960706+mammothb@users.noreply.github.com>
2022-01-01 18:31:05 +0800

    docs: update benchmarks

--- README.md
+++ README.md
@@ -3,8 +3,10 @@ levenjs <br>
 [![Tests](https://github.com/mammothb/levenjs/actions/workflows/tests.yml/badge.svg)](https://github.com/mammothb/levenjs/actions/workflows/tests.yml)
 =============
 
-Fast implementation of the Levenshtein edit distance. The original C# project 
-can be found at [SoftWx.Match](https://github.com/softwx/SoftWx.Match).
+Fast implementation of the Levenshtein edit distance. References for the various
+function can be found in the docstrings [[1](https://github.com/softwx/SoftWx.Match),
+[2](https://github.com/maxbachmann/rapidfuzz-cpp),
+[3](https://github.com/ka-weihe/fastest-levenshtein)].
 
 ## Install
 ```
@@ -21,38 +23,46 @@ If you want to limit the maximum edit distance returned.
 ```js
 import levenjs from "levenjs";
 
-levenjs("distance", "dist", /*maxDistance=*/ 2);  // 2
+levenjs("distance", "dist", /*maxDist=*/ 2);  // 2
 ```
 
 ## Benchmark
-Benchmarked against [`ukkonen`](https://github.com/sunesimonsen/ukkonen) and the
-fastest implementation of Levenshtein edit distance on NPM
-[`leven`](https://github.com/sindresorhus/leven).
+Benchmarked against [`leven`](https://github.com/sindresorhus/leven) and
+[`fastest-levenshtein`](https://github.com/ka-weihe/fastest-levenshtein).
 ```
 One word
-         ukkonen x 429,453 ops/sec ±0.25% (97 runs sampled)
-         leven x 660,136 ops/sec ±0.18% (97 runs sampled)
-         levenjs x 617,170 ops/sec ±0.16% (99 runs sampled)
-Sentence, small difference
-         ukkonen x 1,218,868 ops/sec ±0.21% (98 runs sampled)
-         leven x 212,308 ops/sec ±0.19% (98 runs sampled)
-         levenjs x 214,374 ops/sec ±0.75% (95 runs sampled)
+         leven               x 646,822 ops/sec ±0.64% (98 runs sampled)
+         fastest-levenshtein x 1,529,761 ops/sec ±0.76% (95 runs sampled)
+         levenjs             x 1,331,218 ops/sec ±0.34% (97 runs sampled)
+Short sentence, small difference
+         leven               x 868,577 ops/sec ±0.63% (90 runs sampled)
+         fastest-levenshtein x 6,881,822 ops/sec ±0.48% (97 runs sampled)
+         levenjs             x 5,578,840 ops/sec ±0.98% (93 runs sampled)
+Short sentence, similar prefix and suffix, small difference
+         leven               x 2,242,494 ops/sec ±0.63% (92 runs sampled)
+         fastest-levenshtein x 4,920,882 ops/sec ±0.39% (98 runs sampled)
+         levenjs             x 6,336,442 ops/sec ±0.57% (92 runs sampled)
 Long text, small difference
-         ukkonen x 154,164 ops/sec ±0.25% (95 runs sampled)
-         leven x 1,034 ops/sec ±0.16% (98 runs sampled)
-         levenjs x 1,028 ops/sec ±0.46% (97 runs sampled)
+         leven               x 1,011 ops/sec ±0.67% (95 runs sampled)
+         fastest-levenshtein x 10,350 ops/sec ±0.47% (97 runs sampled)
+         levenjs             x 10,796 ops/sec ±0.42% (97 runs sampled)
+Long text, small difference, threshold 3
+         leven               x 894 ops/sec ±0.41% (97 runs sampled)
+         fastest-levenshtein x 10,374 ops/sec ±1.31% (96 runs sampled)
+         levenjs             x 397,756 ops/sec ±0.52% (97 runs sampled)
+Long text, small difference, threshold 20
+         leven               x 1,030 ops/sec ±0.43% (97 runs sampled)
+         fastest-levenshtein x 10,312 ops/sec ±0.55% (97 runs sampled)
+         levenjs             x 17,998 ops/sec ±0.41% (94 runs sampled)
 Long text, many difference
-         ukkonen x 576 ops/sec ±0.16% (94 runs sampled)
-         leven x 630 ops/sec ±0.11% (97 runs sampled)
-         levenjs x 641 ops/sec ±0.20% (96 runs sampled)
-Long text, small difference, threshold 10
-         ukkonen x 194,398 ops/sec ±0.38% (97 runs sampled)
-         leven x 1,035 ops/sec ±0.23% (97 runs sampled)
-         levenjs x 18,689 ops/sec ±0.24% (100 runs sampled)
+         leven               x 622 ops/sec ±0.58% (96 runs sampled)
+         fastest-levenshtein x 7,422 ops/sec ±0.43% (98 runs sampled)
+         levenjs             x 6,342 ops/sec ±0.28% (99 runs sampled)
 Long text, many difference, threshold 40
-         ukkonen x 123,123 ops/sec ±0.18% (99 runs sampled)
-         leven x 635 ops/sec ±0.12% (95 runs sampled)
-         levenjs x 191,112 ops/sec ±0.15% (96 runs sampled)
+         leven               x 634 ops/sec ±0.25% (95 runs sampled)
+         fastest-levenshtein x 7,879 ops/sec ±0.26% (99 runs sampled)
+         levenjs             x 169,151 ops/sec ±0.50% (95 runs sampled)
 ```
-Other than single word cases, mostly comparable performance with `leven`,
-better performance when `maxDistance` is set.
+Slightly poorer performance than `fastest-levenshtein` in uncapped cases but
+better performance when pre-/post- fix trimming shortens string lengths past
+certain break points. Also better performance when a `maxDist` threshold is set.
--- bench.js
+++ bench.js
@@ -3,7 +3,7 @@
 import Benchmark from "benchmark";
 import levenjs from "./index.js";
 import leven from "leven";
-import ukkonen from "ukkonen";
+import { distance } from "fastest-levenshtein";
 
 const oneWord = (fn) => {
   fn("a", "b");
@@ -22,10 +22,14 @@ const oneWord = (fn) => {
   fn("因為我是中國人所以我會說中文", "因為我是英國人所以我會說英文");
 };
 
-const sentenceSmallDiff = (fn) => {
+const shortTextSmallDiff = (fn) => {
+  fn("Lore Isum is dummy txt!", "Eorem Ifpsum is dummy text.");
+};
+
+const shortTextSimilarSmallDiff = (fn) => {
   fn(
-    "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-    "Lorem Ipsum is simply clever text of the printing and typesetting industries."
+    "Lorem Ipsu is imply ummy text.",
+    "Lorem Ipsurm ies simplyc dbuammy text."
   );
 };
 
@@ -36,14 +40,15 @@ const longTextSmallDiff = (fn) => {
   );
 };
 
-const longTextManyDiff = (fn) => {
+const longTextMax3 = (fn) => {
   fn(
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras tellus sapien, rhoncus sed bibendum in, facilisis non urna. Cras non mattis tellus, nec facilisis nisi. Proin vel purus eros. Morbi ultrices egestas mi vitae laoreet. Ut feugiat est lorem, a rhoncus mi lacinia vel. Aenean et velit neque. Quisque accumsan mi ligula, eu placerat lorem elementum ac. Nunc congue, eros eu aliquam commodo, leo orci tristique nulla, eu tempus quam justo eu neque. Nulla purus elit, porttitor ut sollicitudin sed, dictum vel justo. Mauris orci nisi, lacinia dictum augue nec, condimentum suscipit metus. Etiam lacinia pretium luctus. Mauris nulla turpis, suscipit vitae lobortis quis, tempor sed ex. Sed elementum enim eget venenatis mollis. Etiam sed congue neque, id tristique ex. Duis vitae ipsum nec ligula vulputate ullamcorper. Phasellus fringilla odio turpis, eu condimentum turpis scelerisque quis.",
-    "Curabitur fringilla eros lacus, et placerat magna pretium in. Suspendisse ut egestas dui. Nam quis sapien eget enim interdum interdum. Phasellus metus ligula, lacinia at tellus eu, iaculis blandit libero. Proin risus sem, ornare a orci et, aliquam rutrum elit. Aenean ac posuere justo, a maximus orci. In molestie nibh quis libero elementum, vel pellentesque metus volutpat. Maecenas non quam felis. Proin congue aliquet mauris laoreet viverra. Fusce auctor sapien a neque varius pellentesque. Nam ut sem neque. Pellentesque bibendum aliquet consectetur. Nam finibus diam non vestibulum maximus. Integer aliquet mattis elit, vitae vehicula erat pulvinar at. Ut placerat viverra aliquam. Nulla vehicula hendrerit justo."
+    "Lorem Ipsum dolor sit amet, cnsectetur elite adipiscing. Cras tellus sapien, rhoncus sed bibendum in, facilisis non urna. Cras non mattis tellus, nec facilisis nisi. Proin vel purus eros. Morbi ultrices egestas mi vitae laoreet. Ut feugiat est lorem, a rhoncus mi lacinia vel. Aenean et velit neque. Quisque accumsan mi ligula, placerat lorem elementum ac. Nunc congue, eros eu aliquam commodo, leo orci tristique nulla, eu tempus quam justo eu neque. Nulla purus elit, porttitor ut sollicitudin sed, dictum vel justo. Mauris orci nisi, lacinia dictum augue nec, condimentum suscipit metus. Etiam lacinia pretium luctus. Mauris nulla turpis, suscipit vitae lobortis quis, tempor sed ex. Sed elementum enim eget venenatis mollis. Etiam sed congue neque, id tristique ex. Duis Vitae jpsum nec ligula vulputate ullamcorper. Phasellus fringilla odio turpis, deu condimentum turpis scelerisque quis.",
+    "Lorem Ipsum dolor sit amet, consectetur elit adipiscing. Cras tellus sapien, rhoncus sed bibendum in, facilisis non urna. Cras non mattis tellus, nec facilisis nisi. Proin vel purus eros. Morbi ultrices egestas mi vitae laoreet. Ut feugiat est lorem, a rhoncus mi lacinia vel. Aenean et velit neque. Quisque accumsan mi ligula, placerat lorem elementum ac. Nunc congue, eros eu aliquam commodo, leo orci tristique nulla, eu tempus quam justo eu neque. Nulla purus elit, porttitor ut sollicitudin sed, dictum vel justo. Mauris orci nisi, lacinia dictum augue nec, condimentum suscipit metus. Etiam lacinia pretium luctus. Mauris nulla turpis, suscipit vitae lobortis quis, tempor sed ex. Sed elementum enim eget venenatis mollis. Etiam sed congue neque, id tristique ex. Duis Vitae ipsum nec ligula vulputate ullamcorper. Phasellus fringilla odio turpis, eu condimentum turpis scelerisque quis.",
+    3
   );
 };
 
-const longTextMaxDistance = (fn) => {
+const longTextMax20 = (fn) => {
   fn(
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras tellus sapien, rhoncus sed bibendum in, facilisis non urna. Cras non mattis tellus, nec facilisis nisi. Proin vel purus eros. Morbi ultrices egestas mi vitae laoreet. Ut feugiat est lorem, a rhoncus mi lacinia vel. Aenean et velit neque. Quisque accumsan mi ligula, eu placerat lorem elementum ac. Nunc congue, eros eu aliquam commodo, leo orci tristique nulla, eu tempus quam justo eu neque. Nulla purus elit, porttitor ut sollicitudin sed, dictum vel justo. Mauris orci nisi, lacinia dictum augue nec, condimentum suscipit metus. Etiam lacinia pretium luctus. Mauris nulla turpis, suscipit vitae lobortis quis, tempor sed ex. Sed elementum enim eget venenatis mollis. Etiam sed congue neque, id tristique ex. Duis vitae ipsum nec ligula vulputate ullamcorper. Phasellus fringilla odio turpis, eu condimentum turpis scelerisque quis.",
     "Lorem Ipsum dolor sit amet, consectetur elit adipiscing. Cras tellus sapien, rhoncus sed bibendum in, facilisis non urna. Cras non mattis tellus, nec facilisis nisi. Proin vel purus eros. Morbi ultrices egestas mi vitae laoreet. Ut feugiat est lorem, a rhoncus mi lacinia vel. Aenean et velit neque. Quisque accumsan mi ligula, placerat lorem elementum ac. Nunc congue, eros eu aliquam commodo, leo orci tristique nulla, eu tempus quam justo eu neque. Nulla purus elit, porttitor ut sollicitudin sed, dictum vel justo. Mauris orci nisi, lacinia dictum augue nec, condimentum suscipit metus. Etiam lacinia pretium luctus. Mauris nulla turpis, suscipit vitae lobortis quis, tempor sed ex. Sed elementum enim eget venenatis mollis. Etiam sed congue neque, id tristique ex. Duis Vitae ipsum nec ligula vulputate ullamcorper. Phasellus fringilla odio turpis, eu condimentum turpis scelerisque quis.",
@@ -51,7 +56,14 @@ const longTextMaxDistance = (fn) => {
   );
 };
 
-const longTextManyDiffMaxDistance = (fn) => {
+const longTextManyDiff = (fn) => {
+  fn(
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras tellus sapien, rhoncus sed bibendum in, facilisis non urna. Cras non mattis tellus, nec facilisis nisi. Proin vel purus eros. Morbi ultrices egestas mi vitae laoreet. Ut feugiat est lorem, a rhoncus mi lacinia vel. Aenean et velit neque. Quisque accumsan mi ligula, eu placerat lorem elementum ac. Nunc congue, eros eu aliquam commodo, leo orci tristique nulla, eu tempus quam justo eu neque. Nulla purus elit, porttitor ut sollicitudin sed, dictum vel justo. Mauris orci nisi, lacinia dictum augue nec, condimentum suscipit metus. Etiam lacinia pretium luctus. Mauris nulla turpis, suscipit vitae lobortis quis, tempor sed ex. Sed elementum enim eget venenatis mollis. Etiam sed congue neque, id tristique ex. Duis vitae ipsum nec ligula vulputate ullamcorper. Phasellus fringilla odio turpis, eu condimentum turpis scelerisque quis.",
+    "Curabitur fringilla eros lacus, et placerat magna pretium in. Suspendisse ut egestas dui. Nam quis sapien eget enim interdum interdum. Phasellus metus ligula, lacinia at tellus eu, iaculis blandit libero. Proin risus sem, ornare a orci et, aliquam rutrum elit. Aenean ac posuere justo, a maximus orci. In molestie nibh quis libero elementum, vel pellentesque metus volutpat. Maecenas non quam felis. Proin congue aliquet mauris laoreet viverra. Fusce auctor sapien a neque varius pellentesque. Nam ut sem neque. Pellentesque bibendum aliquet consectetur. Nam finibus diam non vestibulum maximus. Integer aliquet mattis elit, vitae vehicula erat pulvinar at. Ut placerat viverra aliquam. Nulla vehicula hendrerit justo."
+  );
+};
+
+const longTextManyDiff40 = (fn) => {
   fn(
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras tellus sapien, rhoncus sed bibendum in, facilisis non urna. Cras non mattis tellus, nec facilisis nisi. Proin vel purus eros. Morbi ultrices egestas mi vitae laoreet. Ut feugiat est lorem, a rhoncus mi lacinia vel. Aenean et velit neque. Quisque accumsan mi ligula, eu placerat lorem elementum ac. Nunc congue, eros eu aliquam commodo, leo orci tristique nulla, eu tempus quam justo eu neque. Nulla purus elit, porttitor ut sollicitudin sed, dictum vel justo. Mauris orci nisi, lacinia dictum augue nec, condimentum suscipit metus. Sed elementum enim eget venenatis mollis. Etiam sed congue neque, id tristique ex. Duis vitae ipsum nec ligula vulputate ullamcorper. Phasellus fringilla odio turpis, eu condimentum turpis scelerisque quis.",
     "Curabitur fringilla eros lacus, et placerat magna pretium in. Suspendisse ut egestas dui. Nam quis sapien eget enim interdum interdum. Phasellus metus ligula, lacinia at tellus eu, iaculis blandit libero. Proin risus sem, ornare a orci et, aliquam rutrum elit. Aenean ac posuere justo, a maximus orci. In molestie nibh quis libero elementum, vel pellentesque metus volutpat. Maecenas non quam felis. Proin congue aliquet mauris laoreet viverra. Fusce auctor sapien a neque varius pellentesque. Nam ut sem neque. Pellentesque bibendum aliquet consectetur. Nam finibus diam non vestibulum maximus. Integer aliquet mattis elit, vitae vehicula erat pulvinar at. Ut placerat viverra aliquam. Nulla vehicula hendrerit justo. Contrary to popular belief, Lorem Ipsum is not simply random text.",
@@ -67,85 +79,117 @@ function onCycle(event) {
   console.log(`\t ${String(event.target)}`);
 }
 
+const fastestLevenshteinName = "fastest-levenshtein";
+const levenName = "leven".padEnd(fastestLevenshteinName.length, " ");
+const levenjsName = "levenjs".padEnd(fastestLevenshteinName.length, " ");
+
 Benchmark.Suite("One word")
-  .add("ukkonen", () => {
-    oneWord(ukkonen);
-  })
-  .add("leven", () => {
+  .add(levenName, () => {
     oneWord(leven);
   })
-  .add("levenjs", () => {
+  .add(fastestLevenshteinName, () => {
+    oneWord(distance);
+  })
+  .add(levenjsName, () => {
     oneWord(levenjs);
   })
   .on("start", onStart)
   .on("cycle", onCycle)
   .run();
 
-Benchmark.Suite("Sentence, small difference")
-  .add("ukkonen", () => {
-    sentenceSmallDiff(ukkonen);
+Benchmark.Suite("Short sentence, small difference")
+  .add(levenName, () => {
+    shortTextSmallDiff(leven);
   })
-  .add("leven", () => {
-    sentenceSmallDiff(leven);
+  .add(fastestLevenshteinName, () => {
+    shortTextSmallDiff(distance);
   })
-  .add("levenjs", () => {
-    sentenceSmallDiff(levenjs);
+  .add(levenjsName, () => {
+    shortTextSmallDiff(levenjs);
+  })
+  .on("start", onStart)
+  .on("cycle", onCycle)
+  .run();
+
+Benchmark.Suite("Short sentence, similar prefix and suffix, small difference")
+  .add(levenName, () => {
+    shortTextSimilarSmallDiff(leven);
+  })
+  .add(fastestLevenshteinName, () => {
+    shortTextSimilarSmallDiff(distance);
+  })
+  .add(levenjsName, () => {
+    shortTextSimilarSmallDiff(levenjs);
   })
   .on("start", onStart)
   .on("cycle", onCycle)
   .run();
 
 Benchmark.Suite("Long text, small difference")
-  .add("ukkonen", () => {
-    longTextSmallDiff(ukkonen);
-  })
-  .add("leven", () => {
+  .add(levenName, () => {
     longTextSmallDiff(leven);
   })
-  .add("levenjs", () => {
+  .add(fastestLevenshteinName, () => {
+    longTextSmallDiff(distance);
+  })
+  .add(levenjsName, () => {
     longTextSmallDiff(levenjs);
   })
   .on("start", onStart)
   .on("cycle", onCycle)
   .run();
 
-Benchmark.Suite("Long text, many difference")
-  .add("ukkonen", () => {
-    longTextManyDiff(ukkonen);
+Benchmark.Suite("Long text, small difference, threshold 3")
+  .add(levenName, () => {
+    longTextMax3(leven);
   })
-  .add("leven", () => {
+  .add(fastestLevenshteinName, () => {
+    longTextMax3(distance);
+  })
+  .add(levenjsName, () => {
+    longTextMax3(levenjs);
+  })
+  .on("start", onStart)
+  .on("cycle", onCycle)
+  .run();
+
+Benchmark.Suite("Long text, small difference, threshold 20")
+  .add(levenName, () => {
+    longTextMax20(leven);
+  })
+  .add(fastestLevenshteinName, () => {
+    longTextMax20(distance);
+  })
+  .add(levenjsName, () => {
+    longTextMax20(levenjs);
+  })
+  .on("start", onStart)
+  .on("cycle", onCycle)
+  .run();
+
+Benchmark.Suite("Long text, many difference")
+  .add(levenName, () => {
     longTextManyDiff(leven);
   })
-  .add("levenjs", () => {
+  .add(fastestLevenshteinName, () => {
+    longTextManyDiff(distance);
+  })
+  .add(levenjsName, () => {
     longTextManyDiff(levenjs);
   })
   .on("start", onStart)
   .on("cycle", onCycle)
   .run();
 
-Benchmark.Suite("Long text, small difference, threshold 10")
-  .add("ukkonen", () => {
-    longTextMaxDistance(ukkonen);
-  })
-  .add("leven", () => {
-    longTextMaxDistance(leven);
-  })
-  .add("levenjs", () => {
-    longTextMaxDistance(levenjs);
-  })
-  .on("start", onStart)
-  .on("cycle", onCycle)
-  .run();
-
 Benchmark.Suite("Long text, many difference, threshold 40")
-  .add("ukkonen", () => {
-    longTextManyDiffMaxDistance(ukkonen);
+  .add(levenName, () => {
+    longTextManyDiff40(leven);
   })
-  .add("leven", () => {
-    longTextManyDiffMaxDistance(leven);
+  .add(fastestLevenshteinName, () => {
+    longTextManyDiff40(distance);
   })
-  .add("levenjs", () => {
-    longTextManyDiffMaxDistance(levenjs);
+  .add(levenjsName, () => {
+    longTextManyDiff40(levenjs);
   })
   .on("start", onStart)
   .on("cycle", onCycle)
